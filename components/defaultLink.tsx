0c5fb29c2e196f18da5de8bf94657d96ac39bbda
bhagle <ben.hagle@gmail.com>
2021-04-11 17:37:37 -0400

    Change span to a in link. Why did I do this?

--- components/defaultLink.tsx
+++ components/defaultLink.tsx
@@ -2,8 +2,8 @@ import NextLink, {LinkProps} from "next/link";
 
 export default function DefaultLink(props: React.PropsWithChildren<LinkProps>) {
     return <NextLink {...props} >
-        <span className={`underline text-blue-600 hover:text-blue-800 visited:text-purple-600`}>
+        <a className={`underline text-blue-600 hover:text-blue-800 visited:text-purple-600`}>
             {props.children}
-        </span>
+        </a>
     </NextLink>
 }
